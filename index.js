8fcb3079397be36506b0d4272a465a845e13a3bc
Mstrodl <ipadlover8322@gmail.com>
2017-03-24 21:16:00 -0400

    Container management in commands

--- index.js
+++ index.js
@@ -57,11 +57,12 @@ bot.on('ready', function () {
 let timers = {};
 
 bot.on('message', function (user, userID, channelID, message, event) {
-  let t = message.split(" ")[0];
+  if(message.charAt(0) != "<" && message.charAt(0) != ">") return false; // Ignore non-commands...
+  let t = message.split(" ")[0].substr(1); // Remove 1st character
   let c = channelID;
   let a = message.split(" ");
   a.shift();
-  if (t == "<eval" || t == ">eval") {
+  if (t == "eval") {
     if (timers[userID]) {
       if (new Date().getTime() - timers[userID] < 0 * 1000) {
         say("Rate limited", c);
@@ -84,7 +85,55 @@ bot.on('message', function (user, userID, channelID, message, event) {
       say("```\n" + tmplog.substring(0, 1980) + "```" + String(new Date().getTime() - begin) + "MS", c);
     });
   }
-  if (t == "<reset" || t == "<respawn" || t == ">respawn" || t == ">reset") {
+  if(t == "snaps") {
+    child_process.exec("lxc-snapshot -n " + vmname + " -L", function(error, stdout, stderrr) {
+      say("Available snapshots:```\n" + stdout + "```", c);
+    });
+  }
+  if (t == "snap") {
+    exec("lxc-snapshot -n " + vmname, () => { }, function (error) {
+      say(":ok_hand: Made a new snapshot!", c);
+    });
+  }
+  if (t == "load-snap") {
+    let snap = a[0];
+    if (!snap) {
+      say("Oh snap! You need to give me a snapshot to load goofy!", c);
+    }
+    exec("lxc-stop -n " + vmname, () => { }, function () {
+      exec("lxc-snapshot -r " + vmname + " -r " + snap, () => { }, function (error) {
+        exec("lxc-start -n " + vmname + " -d", () => { }, function () {
+          say("Loaded snapshot!", c);
+        });
+      });
+    });
+  }
+  if (t == "container") {
+    let cmdarg = a;
+    // let name = cmdarg.shift();
+    let template = cmdarg.shift();
+    say(":ok_hand: This may take a while! In the meantime, `eval` won't work...", c);
+    child_process.exec("lxc-stop -n " + vmname, function (e_1, e_2, e_3) {
+      console.log("Stopped VM");
+      child_process.exec("lxc-destroy -fsn " + vmname, function (e_4, e_5, e_6) {
+        console.log("Destroyed VM");
+        child_process.exec("lxc-create -n " + vmname + " -t " + template + " -- " + cmdarg.join(" "), function (e_7, e_8, e_9) {
+          console.log("Created new VM");
+          child_process.exec("lxc-snapshot -n " + vmname, function (e_10, e_11, e_12) {
+            console.log("Created snapshot");
+            child_process.exec("lxc-start -n " + vmname, function (e_13, e_14, e_15) {
+              console.log(e_1, e_2, e_3, e_4, e_5, e_6, e_7, e_8, e_9, e_10, e_11, e_12, e_13, e_14, e_15);
+              console.log("Started up new VM!");
+              say("Purged old container!\n\
+                  Created new container using template **" + template + "** using the arguments `" + cmdarg.join(" ") + "`\n\
+                  Also created the `snap0` snapshot!", c);
+            });
+          });
+        });
+      });
+    });
+  }
+  if (t == "reset" || t == "respawn") {
     if (resetting) {
       say("Nope! Nice try Jake! You can't run 2 resets at once!", c);
       return false;
@@ -100,7 +149,7 @@ bot.on('message', function (user, userID, channelID, message, event) {
       });
     })
   }
-  if (t == "<reboot" || t == ">reboot") {
+  if (t == "reboot") {
     say(":alarm_clock: Deleting Jake-limits... Just a sec...", c);
     let tmplog = "";
     exec("lxc-stop -n " + vmname, function (output) { tmplog += output }, function () {
@@ -109,19 +158,23 @@ bot.on('message', function (user, userID, channelID, message, event) {
       });
     });
   }
-  if (t == "<ping" || t == ">ping") {
+  if (t == "ping") {
     say("http://takeb1nzyto.space", c);
   }
-  if (t == "<ip" || t == ">ip") {
+  if (t == "ip") {
     say("You can reach any HTTP service that runs on port 80 here: http://osjs.strodl.tk", c);
   }
-  if (t == "<help" || t == ">help") {
+  if (t == "help") {
     say(":wave: I'm ~~a slave which you run experiments on~~ a bot that's designed to act similarly to B1nzy's bot (With a few key differences including an intentional lack of rate-limits, support for forking, and multiple processes running concurrently :smiley:\n\
     These are my commands. You can use `>` or `<` to prefix them:\n\
     `eval` - runs a given program on the VM\n\
     `respawn` / `reset` - resets the VM to a fresh copy\n\
     `reboot` - reboots the VM\n\
     `ip` - gives the link to the VM's public facing HTTP server\n\
+    `container` - Usage: container (Template) (ARGS) creates a new container of the given template and appends args to the command\n\
+    `snap` - Creates a new snapshot\n\
+    `load-snap` - Loads the given snapshot\n\
+    `snaps` - Gives the list of snapshots for the given container\n\
     `ping` - ????", c);
   }
 });
